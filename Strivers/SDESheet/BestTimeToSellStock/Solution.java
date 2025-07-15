af0a231d6d18aab51dfc8aad049e64e5bb09fd8b
shaikh-sakir <sakir-shaikh>
2025-07-15 20:49:51 +0530

    day 2 best time to buy and sell stock

---
+++ Strivers/SDESheet/BestTimeToSellStock/Solution.java
@@ -0,0 +1,20 @@
+
+
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int min=prices[0];
+        for(int i=1; i<prices.length; i++){
+            if(min < prices[i]){
+                maxProfit = Math.max(maxProfit, prices[i]-min);
+            }else{
+                min=prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
+
+
